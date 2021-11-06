86845c6441009edc533ae13e61900f39240a87ca
FIRAT <frtdkmn@gmail.com>
2021-11-06 22:03:07 +0300

    Lesson 04 added code Part-1

--- Lesson04/navigationSample/App.js
+++ Lesson04/navigationSample/App.js
@@ -1,14 +1,38 @@
-import React, { Component } from 'react'
-import { Text, StyleSheet, SafeAreaView } from 'react-native'
+import React from 'react'
+import { StyleSheet, Text, View ,SafeAreaView} from 'react-native'
+import {createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native';
 
-export default class App extends Component {
-  render() {
-    return (
-      <SafeAreaView>
-        <Text> Hello world! </Text>
-      </SafeAreaView>
-    )
-  }
+
+function HomeScreen() {
+  return (
+    <SafeAreaView style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+      <Text>Home Screen</Text>
+    </SafeAreaView>
+  )
+}
+
+
+function DetailsScreen() {
+  return (
+    <SafeAreaView style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+      <Text>Details Screen</Text>
+    </SafeAreaView>
+  )
+}
+
+const Stack=createNativeStackNavigator();
+
+export default function App() {
+  return (
+   <NavigationContainer>
+     <Stack.Navigator initialRouteName="Home" screenOptions={{headerStyle:{backgroundColor:'red',color:'white'}}}>
+       <Stack.Screen name="Home" component={HomeScreen} options={{title:'Home Title'}}/>
+       <Stack.Screen name="Details" component={DetailsScreen}/>
+       {/* <Stack.Screen name="Details" component={()=><DetailsScreen/>}/> */}
+     </Stack.Navigator>
+   </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({})
