b30ff07e1a506e71cda2bd240d24e526bcd96ba9
FIRAT <frtdkmn@gmail.com>
2021-11-10 17:41:06 +0300

    Added hello-world app for image-picker.

---
+++ Lesson05/imagePickerSample/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, SafeAreaView } from 'react-native'
+
+export default class App extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text> Hello World!!! </Text>
+      </SafeAreaView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
