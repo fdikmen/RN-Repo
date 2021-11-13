1fdd6255ed29479b9f66d8f53a8988fc66113f5f
FIRAT <frtdkmn@gmail.com>
2021-11-13 19:28:25 +0300

    Added hello-world project for Animations.

---
+++ Lesson06/animationSample/App.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'react'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+
+  return (
+    <SafeAreaView>
+      <Text>Hello World!!!</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({})
