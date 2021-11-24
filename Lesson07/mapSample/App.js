37f11d403258b4701e9e4e8c9dbf086a51a1e46b
FIRAT <frtdkmn@gmail.com>
2021-11-24 17:36:58 +0300

    Configuration for IOS

--- Lesson07/mapSample/App.js
+++ Lesson07/mapSample/App.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Map Sample</Text>
+      <MapView
+      provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {flex: 1 }
+  container: {flex: 1 },
+  map: {flex: 1}
 });
