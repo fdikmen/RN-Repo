4035413c88fe646c15ba425c47bdb4fc1c71a1e8
FIRAT <frtdkmn@gmail.com>
2021-11-24 16:52:19 +0300

    Added first hello-world-project for Map-Sample.

---
+++ Lesson07/mapSample/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Map Sample</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {flex: 1 }
+});
